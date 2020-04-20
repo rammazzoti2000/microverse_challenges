f7bdf62a9880ea9db02a8284ce29266af5fe1dd3
Alexandru <rammazzoti2000@gmail.com>
2020-04-20 11:25:59 +0200

    add mandatory warm up challenges

--- Mandatory/03_Anagram.rb
+++ Mandatory/03_Anagram.rb
@@ -1,26 +1,3 @@
-# FizzzBuzz
-100.times do |i|
-  i+=1
-  if i%15==0
-      puts "FizzBuzz"
-  elsif i%5==0
-      puts "Buzz"
-  elsif i%3==0
-      puts "Fizz"
-  else
-      puts i
-  end
-end
-
-# RepeatedString
-def repeatedString(s, n)
-    
-  return n if s.size == 1 && s == 'a'
-
-  total = (s.count('a') * (n/s.size)) + s[0, n % s.size].count('a')
-  total
-end
-
 #Anagram
 def anagram(s)
   s_left = s[0...s.length/2.0].split('')
